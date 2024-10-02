01d38dd3ae7adfcc4dc3ce8d0305be011b4d4a11
Vladimir Zdorenko <vovchikzd@gmail.com>
2024-10-02 14:49:27 +0300

    write working init.lua

--- init.lua
+++ init.lua
@@ -1,30 +1,42 @@
 local M = {}
 
 function M:peek()
-  local image_height = 0 -- for later
+	local cache = ya.file_cache(self)
+	if not cache then
+		return
+	end
 
-  if self:preload() == 1 then
-    local cache = ya.file_cache(self)
-    if cache and fs.cha(cache).length > 0 then
-      image_height = ya.image_show(cache, self.area).h
-    end
-  end
-
-
-  local cmd = ""
-  local output, err = Command(cmd)
-    :args({
-      cmd
-      , "info"
-      , tostring(self.file.url)
-    })
-    :stdout(Command.PIPED)
-    :output()
-
+	if self:preload() == 1 then
+    ya.err("cache: " .. tostring(cache))
+		ya.image_show(cache, self.area)
+		ya.preview_widgets(self, {})
+	end
 end
 
 function M:seek() end
 
-function M:preload() end
+function M:preload()
+	local cache = ya.file_cache(self)
+	if not cache or fs.cha(cache) then
+		return 1
+	end
+
+	local size = math.min(PREVIEW.max_width, PREVIEW.max_height)
+
+	local child, code = Command("/home/vovchik/Disks/1Tb/projects/ebook-preview.yazi/ebook-preview.py"):args({
+    "c",
+		tostring(self.file.url),
+		tostring(cache),
+		tostring(size),
+	}):spawn()
+
+	if not child then
+		ya.err("spawn `ebook-preview` command returns " .. tostring(code))
+		return 0
+	end
+
+	local status = child:wait()
+	return status and status.success and 1 or 2
+end
 
 return M
